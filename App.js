f68dba2386108e0c2eb1b2a08156949d272532ec
unknown <marjorylourenco@gmail.com>
2024-03-14 10:39:10 -0300

    nome do commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Marjory Lourenço</Text>
+      <Text>14/04/2024</Text>
+      <br></br>
+      <Button title='Salvar'></Button> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
