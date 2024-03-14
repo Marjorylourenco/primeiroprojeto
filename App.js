5ccfed2e1785cc1c64965c2abc31600efba19f57
Marjorylourenco <marjorylourenco@gmail.com>
2024-03-14 10:49:50 -0300

    botão cancelar

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default function App() {
       <br></br>
       <Button title='Salvar'></Button> 
        <Button title='Entrar'></Button> 
+       <Button title='Cancelar'></Button> 
     </View>
   );
 }
